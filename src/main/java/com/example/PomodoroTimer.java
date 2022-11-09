8c783e0777891a247efaff3fc26ac5e442847016
proux <proux03@outlook.com>
2022-11-09 21:55:11 +0800

    added count and multiplier(with error)

--- src/main/java/com/example/PomodoroTimer.java
+++ src/main/java/com/example/PomodoroTimer.java
@@ -10,18 +10,35 @@ public class PomodoroTimer {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Введите время работы и отдыха");
+        //test input: -w 2 -b 3 -c 3 -m 2
         String[] input = new Scanner(System.in).nextLine().split(" ");
 
         int workTime = 1;
         int breakTime = 1;
+        int count = 1;
+        int multiplier = 1;
+
         boolean isHelp = false;
 
         for(int i = 0; i < input.length; i++) {
             switch (input[i]) {
                 case "-w" -> workTime = Integer.parseInt(input[++i]);
                 case "-b" -> breakTime = Integer.parseInt(input[++i]);
+                case "-c" -> count = Integer.parseInt(input[++i]);
+                case "-m" -> multiplier = Integer.parseInt(input[++i]);
                 case "--help" -> {
-                    System.out.println("help");
+                    System.out.println(
+                            "\n\nPomodoro - сделай свое время более эффективным\n");
+                    System.out.println(
+                            "	-w <workTime>: время работы (сколько хочешь работать)\n");
+                    System.out.println(
+                            "	-b <breakTime>: время отдыха (сколько хочешь отдыхать)\n");
+                    System.out.println(
+                            "	-c <count>: количество итераций.\n");
+                    System.out.println(
+                            "	-m <multiplier>: множитель времени работы\n");
+                    System.out.println(
+                            "	--help: меню помощи.\n");
                     isHelp = true;
                 }
             }
@@ -29,12 +46,16 @@ public class PomodoroTimer {
         if(isHelp) return;
 
         long startTime = System.currentTimeMillis();
-        timer(workTime, breakTime);
+
+        for(int i = 0; i <= count; i++) {
+            timer(workTime, breakTime);
+            workTime = workTime * multiplier;
+        }
+
         long endTime = System.currentTimeMillis();
 
         System.out.println("Таймер работал " + (endTime - startTime) / (1000 * 60) + " min");
 
-        //System.out.println("workTime = " + workTime + ", breakTime = " + breakTime);
     }
 
     public static void timer(int workTime, int breakTime) throws InterruptedException {
@@ -45,10 +66,10 @@ public class PomodoroTimer {
     private static void printProgress(String process, int time, int size) throws InterruptedException {
         int length;
         int rep;
-        length = 60* time / size;
-        rep = 60* time /length;
-        int stretchb = size /(3* time);
-        for(int i=1; i <= rep; i++){
+        length = 60 * time / size;
+        rep = 60 * time /length;
+        int stretchb = size / (3 * time);
+        for(int i = 1; i <= rep; i++) {
             double x = i;
             x = 1.0/3.0 *x;
             x *= 10;
